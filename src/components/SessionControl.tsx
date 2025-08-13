2c97e2ee0956cc0e66124c2868f27d85cf554335
jmtalarn <jmtalarn@gmail.com>
2025-08-13 16:48:10 +0200

    Base

--- src/components/SessionControl.tsx
+++ src/components/SessionControl.tsx
@@ -57,7 +57,7 @@ export function SessionControl() {
 		}
 	};
 	const isSessionActive = currentSessionId !== null;
-	const buttonColor = isSessionActive ? 'red' : 'green';
+
 	return (
 		<div className="flex flex-wrap items-end gap-3 ">
 			<div className="flex flex-col gap-1">
@@ -98,12 +98,16 @@ export function SessionControl() {
 				onClick={currentSessionId ? handleStopSession : handleStartSession}
 				className={
 					`inline-flex items-center gap-2 px-4 py-2 text-sm font-medium text-white 
-					bg-${buttonColor}-600 rounded-md hover:bg-${buttonColor}-700 
+					${isSessionActive
+						? 'bg-red-600 hover:bg-red-700 focus:ring-red-400'
+						: 'bg-green-600 hover:bg-green-700 focus:ring-green-400'}
+					rounded-md
 					transition 
-					focus:outline-none focus:ring-2 focus:ring-${buttonColor}-400 
+					focus:outline-none
 					min-w-[180px] justify-center text-center
 					disabled:opacity-50 disabled:cursor-not-allowed
-					`}
+					`
+				}
 				disabled={!selectedProjectId && !currentSessionId}
 			>
 				{currentSessionId ? <><svg className="w-4 h-4 fill-current" viewBox="0 0 20 20">
